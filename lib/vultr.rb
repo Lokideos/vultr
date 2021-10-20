2112a534ad9220485c713498120627fdb17df2b5
lokideos <archmage09@gmail.com>
2021-10-20 20:43:06 +0300

    Parse Vultr response to ruby open structs

--- lib/vultr.rb
+++ lib/vultr.rb
@@ -4,5 +4,11 @@ require_relative "vultr/version"
 
 module Vultr
   autoload :Client, "vultr/client"
+  autoload :Collection, "vultr/collection"
   autoload :Error, "vultr/error"
+  autoload :Object, "vultr/object"
+
+  autoload :Account, "vultr/objects/account"
+  autoload :Instance, "vultr/objects/account"
+  autoload :Plan, "vultr/objects/plan"
 end
